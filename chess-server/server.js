b5491b5c658f5870892ac7b34b25082a153724af
ramarsiva <ramar.siva@gmail.com>
2025-12-06 03:15:46 +0100

    updated favicon and server port

--- chess-server/server.js
+++ chess-server/server.js
@@ -109,7 +109,8 @@ io.on('connection', (socket) => {
 });
 
 
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
